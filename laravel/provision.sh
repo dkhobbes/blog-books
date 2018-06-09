a7f8f02e1011b7a30c15dcbeafc07abe92e49224
David Kurth <david-kurth@sbcglobal.net>
2018-06-09 07:18:28 -0500

    working on provision

--- laravel/provision.sh
+++ laravel/provision.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env #!/usr/bin/env bash
+
+rootpass=$1
+mysqldb=$2
+mysqluser=$3
+mysqlpass=$4
+
+wp_install=$5
+wp_url=$6
+wp_title=$7
+wp_admin=$8
+wp_admin_pass=$9
+wp_admin_email=${10}
+
+#echo "rootpass: $rootpass"
+#echo "mysqldb: $mysqldb"
+#echo "mysqluser: $mysqluser"
+#echo "mysqlpass: $mysqlpass"
